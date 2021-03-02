dd6af9345c5873464cc37741998c7ea88671921f
dred22 <TeLegraph83>
2021-03-02 14:23:35 +0100

    Change to NoSQL MongoDb and Gradle

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -1,10 +1,8 @@
 package guru.springframework.domain;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
 import java.math.BigDecimal;
 
 /**
@@ -12,20 +10,13 @@ import java.math.BigDecimal;
  */
 @Getter
 @Setter
-@EqualsAndHashCode(exclude = {"recipe"})
-@Entity
 public class Ingredient {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
     private String description;
     private BigDecimal amount;
 
-    @OneToOne(fetch = FetchType.EAGER)
     private UnitOfMeasure uom;
-
-    @ManyToOne
     private Recipe recipe;
 
     public Ingredient() {
